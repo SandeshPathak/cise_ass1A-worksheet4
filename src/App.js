3ddd21b115bdce763ce6db28f38ad958874f5d96
SandeshPathak <sandesh.pathak07@gmail.com>
2022-08-21 15:30:57 +1200

    CSS Changes commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Route, NavLink, BrowserRouter as Router, Routes,
+} from "react-router-dom";
+import Home from "./pages/Home";  
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>  {
+    return (
+        <div>
+          <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
+            <ul className="header">
+              <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+            </ul>
+          <div className="content">
+            <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route  path="/SEPractice" element={<SEPractice />}/>
+            <Route  path="/SubmitArticle" element={<SubmitArticle />}/>
+            </Routes>
+          </div>
+
+        </div>
+    );
 }
-
+ 
 export default App;

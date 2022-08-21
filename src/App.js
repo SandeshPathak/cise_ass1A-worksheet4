e8cd5de2ac2182a5c30c7605af53dbb420508331
SandeshPathak <sandesh.pathak07@gmail.com>
2022-08-21 15:58:02 +1200

    404 Page not found setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  Route, NavLink, BrowserRouter as Router, Routes,
+  Route, NavLink, Routes
 } from "react-router-dom";
 import Home from "./pages/Home";  
 import SEPractice from "./pages/SE-Practice";
@@ -12,15 +12,16 @@ const App = () =>  {
         <div>
           <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
             <ul className="header">
-              <li><NavLink exact to = "/">Home</NavLink></li>
+              <li><NavLink to = "/">Home</NavLink></li>
                 <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
                 <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
           <div className="content">
             <Routes>
-            <Route exact path="/" element={<Home />}/>
+            <Route  path="/" element={<Home />}/>
             <Route  path="/SEPractice" element={<SEPractice />}/>
             <Route  path="/SubmitArticle" element={<SubmitArticle />}/>
+            <Route  path="*" element={<NotFoundPage />}/>
             </Routes>
           </div>
 
